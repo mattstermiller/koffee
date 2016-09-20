514e022fe233e5e4144e195e155a9ffd39ac86a8
Matt Miller <miller.mattster@gmail.com>
2016-09-20 18:53:51 -0500

    Empty WPF project.

---
+++ Koffee/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Koffee.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Koffee")>]
+[<assembly: AssemblyDescription("The Keyboard-Oriented File & Folder Explorer for Efficiency, or Koffee, is a minimalistic replacement for Windows Explorer with VIM-like keyboard navigation and usability.")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Koffee")>]
+[<assembly: AssemblyCopyright("Copyright © Matthew A. Miller 2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("4df0ba98-3ce7-422b-b37e-afb2576fa90d")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ Koffee/Program.fs
@@ -0,0 +1,14 @@
+﻿module Program
+
+let assemblyName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name
+
+let getXamlResource xaml = 
+  sprintf "/%s;component/%s" assemblyName xaml
+  |> fun nme -> System.Uri(nme, System.UriKind.Relative)
+  |> System.Windows.Application.LoadComponent
+  :?> _
+
+let mainWindow : System.Windows.Window = getXamlResource "MainWindow.xaml"
+
+[<System.STAThread>]
+do System.Windows.Application().Run(mainWindow) |> ignore

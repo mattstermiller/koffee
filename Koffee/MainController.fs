7f29e9c9066739d5f68f2f8c220d469d5688f31e
Matt Miller <miller.mattster@gmail.com>
2016-09-28 08:05:18 -0500

    Refactored controller let functions into members.

--- Koffee/MainController.fs
+++ Koffee/MainController.fs
@@ -8,19 +8,6 @@ type IPathService =
     abstract nodes: Path -> Node list
 
 type MainController(path: IPathService) =
-    let nav offset (model: MainModel) =
-        model.Cursor <- model.Cursor + offset
-
-    let openPath (model: MainModel) =
-        model.Nodes <- path.nodes model.Path
-        model.Cursor <- 0
-
-    let selectedPath (model: MainModel) =
-        model.Path <- model.SelectedNode.Path
-
-    let parentPath (model: MainModel) =
-        model.Path <- path.parent model.Path
-
     interface IController<MainEvents, MainModel> with
         member this.InitModel model =
             model.Path <- path.root
@@ -28,8 +15,21 @@ type MainController(path: IPathService) =
             model.Cursor <- 0
 
         member this.Dispatcher = function
-            | NavUp -> Sync (nav -1)
-            | NavDown -> Sync (nav 1)
-            | PathChanged -> Sync openPath
-            | OpenSelected -> Sync selectedPath
-            | OpenParent -> Sync parentPath
+            | NavUp -> Sync (this.Nav -1)
+            | NavDown -> Sync (this.Nav 1)
+            | PathChanged -> Sync this.OpenPath
+            | OpenSelected -> Sync this.SelectedPath
+            | OpenParent -> Sync this.ParentPath
+
+    member this.Nav offset (model: MainModel) =
+        model.Cursor <- model.Cursor + offset
+
+    member this.OpenPath (model: MainModel) =
+        model.Nodes <- path.nodes model.Path
+        model.Cursor <- 0
+
+    member this.SelectedPath (model: MainModel) =
+        model.Path <- model.SelectedNode.Path
+
+    member this.ParentPath (model: MainModel) =
+        model.Path <- path.parent model.Path

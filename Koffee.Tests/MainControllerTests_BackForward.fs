649c5506ee819793b09aa9fdace8cd44eeffc392
Matt Miller <miller.mattster@gmail.com>
2016-12-03 13:13:27 -0600

    Split MainControllerTests into separate files.
    Tests are grouped into files by test class, which are tests grouped by similar functions.

--- Koffee.Tests/MainControllerTests_BackForward.fs
+++ Koffee.Tests/MainControllerTests_BackForward.fs
@@ -8,89 +8,8 @@ open Foq
 open Koffee
 
 [<TestFixture>]
-type ``MainController tests for Find and Search``() =
-    let Nodes names =
-        let toNode name = {
-            Name = name
-            Path = Path ""
-            Type = Folder
-            Modified = None
-            Size = None
-        }
-        names |> Seq.map toNode |> Seq.toList
-
-    let CreateModel inputMode cursorStart =
-        let model = MainModel.Create<MainModel>()
-        model.Nodes <- "alice,bob,charlie,crystal,apple,cherry".Split(',') |> Nodes
-        model.Cursor <- cursorStart
-        model.CommandInputMode <- Some inputMode
-        model
-
-    let CreateController =
-        let fileSys = Mock.Of<IFileSystemService>()
-        let settingsFactory () = Mock.Of<Mvc<SettingsEvents, SettingsModel>>()
-        MainController(fileSys, settingsFactory)
-
-    let Find char cursorStart =
-        let model = CreateModel Find cursorStart
-
-        let contr = CreateController
-        contr.CommandCharTyped char model
-
-        model.CommandInputMode |> should equal None
-        model.LastFind |> should equal (Some char)
-        model.Status |> should equal ("Find " + (char.ToString()))
-        model.Cursor
-
-    let Search searchStr cursorStart =
-        let model = CreateModel Search cursorStart
-        model.CommandText <- searchStr
-
-        let contr = CreateController
-        contr.ExecuteCommand model
-
-        model.CommandInputMode |> should equal None
-        model.LastSearch |> should equal (Some searchStr)
-        model.Status |> should equal (sprintf "Search \"%s\"" searchStr)
-        model.Cursor
-
-    [<Test>]
-    member x.``Find a char that matches nothing should not change the cursor``() =
-        Find 'A' 1 |> should equal 1
-
-    [<Test>]
-    member x.``Find a char that matches only the current node should not change the cursor``() =
-        Find 'b' 1 |> should equal 1
-
-    [<Test>]
-    member x.``Find a char that matches the next node should set the cursor to the next index``() =
-        Find 'c' 2 |> should equal 3
-
-    [<Test>]
-    member x.``Find a char that matches a node wrapping around should set the cursor to the that index``() =
-        Find 'b' 2 |> should equal 1
-
-
-    [<Test>]
-    member x.``Search that matches nothing should not change the cursor``() =
-        Search "abc" 1 |> should equal 1
-
-    [<Test>]
-    member x.``Search that matches only the current node should not change the cursor``() =
-        Search "ob" 1 |> should equal 1
-
-    [<Test>]
-    member x.``Search that matches the next node should set the cursor to the next index``() =
-        Search "ry" 2 |> should equal 3
-
-    [<Test>]
-    member x.``Search that matches a node wrapping around should set the cursor to the that index``() =
-        Search "ob" 2 |> should equal 1
-
-
-[<TestFixture>]
 type ``MainController tests for Back and Forward``() =
-    let Controller () =
+    let CreateController () =
         let fileSys =
             Mock<IFileSystemService>()
                 .Setup(fun x -> <@ x.Normalize(any()) @>).Calls<Path>(fun p -> p)
@@ -105,7 +24,8 @@ type ``MainController tests for Back and Forward``() =
         model.ForwardStack <- forwardStack |> List.map (fun (p, c) -> (Path p, c))
         model.Path <- Path pathStr
         model.Cursor <- cursor
-        let contr = Controller()
+        model.Status <- "previous status"
+        let contr = CreateController()
         contrFunc contr model
         model
 
@@ -119,6 +39,7 @@ type ``MainController tests for Back and Forward``() =
         model.Cursor |> should equal 1
         model.BackStack |> should equal []
         model.ForwardStack |> should equal []
+        model.Status |> should equal "previous status"
 
     [<Test>]
     member x.``Back with simple history changes path and stacks``() =
@@ -127,6 +48,7 @@ type ``MainController tests for Back and Forward``() =
         model.Cursor |> should equal 2
         model.BackStack |> should equal []
         model.ForwardStack |> should equal [Path "orig", 1]
+        model.Status |> should equal ""
 
     [<Test>]
     member x.``Back with more history changes path and stacks``() =
@@ -135,6 +57,7 @@ type ``MainController tests for Back and Forward``() =
         model.Cursor |> should equal 2
         model.BackStack |> should equal [Path "back2", 3]
         model.ForwardStack |> should equal [Path "orig", 1; Path "fwd1", 4; Path "fwd2", 5]
+        model.Status |> should equal ""
 
     [<Test>]
     member x.``Forward without history does nothing``() =
@@ -143,6 +66,7 @@ type ``MainController tests for Back and Forward``() =
         model.Cursor |> should equal 1
         model.BackStack |> should equal []
         model.ForwardStack |> should equal []
+        model.Status |> should equal "previous status"
 
     [<Test>]
     member x.``Forward with simple history changes path and stacks``() =
@@ -151,6 +75,7 @@ type ``MainController tests for Back and Forward``() =
         model.Cursor |> should equal 2
         model.BackStack |> should equal [Path "orig", 1]
         model.ForwardStack |> should equal []
+        model.Status |> should equal ""
 
     [<Test>]
     member x.``Forward with more history changes path and stacks``() =
@@ -159,3 +84,4 @@ type ``MainController tests for Back and Forward``() =
         model.Cursor |> should equal 4
         model.BackStack |> should equal [Path "orig", 1; Path "back1", 2; Path "back2", 3]
         model.ForwardStack |> should equal [Path "fwd2", 5]
+        model.Status |> should equal ""

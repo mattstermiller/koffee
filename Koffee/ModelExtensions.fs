98e09df52f38bf09b418d1fcfa3789a7aa241ee0
Matt Miller <miller.mattster@gmail.com>
2017-03-18 12:23:58 -0500

    Extracts PropertyExpression pattern.

--- Koffee/ModelExtensions.fs
+++ Koffee/ModelExtensions.fs
@@ -2,17 +2,16 @@
 
 open System.ComponentModel
 open Microsoft.FSharp.Quotations
-open Microsoft.FSharp.Quotations.Patterns
-open Microsoft.FSharp.Quotations.DerivedPatterns
+open Reflection
 
 type INotifyPropertyChanged with
-    member this.PropertyChangedEvent (propertySelector : Expr<'a>) =
-        match propertySelector with
-        | PropertyGet (Some obj, property, []) when obj.Type.IsAssignableFrom(this.GetType()) ->
+    member this.PropertyChangedEvent (propertyExpr: Expr<'a>) =
+        match propertyExpr with
+        | PropertyExpression (obj, property) when obj.GetType() = this.GetType() ->
             this.PropertyChanged
             |> Observable.filter (fun e -> e.PropertyName = property.Name)
             |> Observable.map (fun e -> property.GetValue this :?> 'a)
-        | _ -> failwith "Invalid propertySelector"
+        | _ -> failwith "Invalid property expression. It must be a property of this model type."
 
     member this.OnPropertyChanged propertySelector handler =
         this.PropertyChangedEvent propertySelector |> Observable.add handler
--- Koffee/Reflection.fs
+++ Koffee/Reflection.fs
@@ -1,7 +1,17 @@
 ﻿module Reflection
 
-open Microsoft.FSharp.Reflection
+open FSharp.Reflection
+open FSharp.Quotations
+open FSharp.Quotations.Patterns
+open FSharp.Quotations.DerivedPatterns
+open FSharp.Quotations.Evaluator
 
 let GetUnionCaseName value =
     match FSharpValue.GetUnionFields(value, value.GetType()) with
     | case, _ -> case.Name
+
+let (|PropertyExpression|_|) expr =
+    match expr with
+    | PropertyGet (Some objExpr, property, []) ->
+        Some (QuotationEvaluator.EvaluateUntyped objExpr, property)
+    | _ -> None

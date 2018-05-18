da3dbf0dc16bbd2a7502241bfd2fb7c289a11326
Matt Miller <miller.mattster@gmail.com>
2018-05-18 11:22:13 -0500

    #44 Adds crash logging and error message.

--- Koffee/Program.fs
+++ Koffee/Program.fs
@@ -20,6 +20,18 @@ let makeMainMvc config options =
     let controller = MainController(fileSys, settingsFactory, window.GetScreenWorkingArea, config, options)
     (Mvc(model, view, controller), window)
 
+let logCrash (e: exn) =
+    let timestamp = System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss")
+    let logFile = Path.KoffeeData.Join(sprintf "crash_%s.log" timestamp).Format Windows
+    System.IO.File.WriteAllText(logFile, string e)
+
+    let msg =
+        "Sorry! An unexpected error caused Koffee to crash:\n\n" +
+        sprintf "%s\n\n" e.Message +
+        sprintf "This error has been logged to: \n%s\n\n" logFile +
+        "Please report this as an issue on Koffee's GitHub project."
+    MessageBox.Show(msg, "Crash!", MessageBoxButton.OK, MessageBoxImage.Error) |> ignore
+
 [<EntryPoint>]
 [<System.STAThread>]
 let main args =
@@ -31,5 +43,6 @@ let main args =
     use eventLoop = mvc.Start()
 
     let app = Application()
-    app.Run window |> ignore
+    try app.Run window |> ignore
+    with e -> logCrash e
     0

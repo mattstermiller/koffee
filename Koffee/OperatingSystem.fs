fb36d1e2b18f2700f9368c7a7475abe1c802eae8
Matt Miller <miller.mattster@gmail.com>
2019-02-23 18:35:40 -0600

    Replaces undocumented DLL entry call with interop.

--- Koffee/OperatingSystem.fs
+++ Koffee/OperatingSystem.fs
@@ -3,6 +3,44 @@
 open System.Diagnostics
 open Acadian.FSharp
 
+module OsInterop =
+    open System
+    open System.Runtime.InteropServices
+
+    // Open With dialog: http://www.pinvoke.net/default.aspx/shell32/SHOpenWithDialog.html
+    module private OpenWithNative =
+        type OpenWithFlags =
+            | ALLOW_REGISTRATION = 0x01 // Show "Always" checkbox
+            | REGISTER_EXT = 0x02       // Perform registration when user hits OK
+            | EXEC = 0x04               // Exec file after registering
+            | FORCE_REGISTRATION = 0x08 // Force the checkbox to be registration
+            | HIDE_REGISTRATION = 0x20  // Vista+: Hide the "always use this file" checkbox
+            | URL_PROTOCOL = 0x40       // Vista+: cszFile is actually a URI scheme; show handlers for that scheme
+            | FILE_IS_URI = 0x80        // Win8+: The location pointed to by the pcszFile parameter is given as a URI
+
+        [<Struct>]
+        type OpenWithInfo =
+            [<MarshalAs(UnmanagedType.LPWStr)>]
+            val mutable cszFile: string
+            [<MarshalAs(UnmanagedType.LPWStr)>]
+            val mutable cszClass: string
+            [<MarshalAs(UnmanagedType.I4)>]
+            val mutable oaifInFlags: OpenWithFlags
+
+        [<DllImport("shell32.dll", EntryPoint = "SHOpenWithDialog", CharSet = CharSet.Unicode)>]
+        extern int SHOpenWithDialog(IntPtr hWndParent, OpenWithInfo& info)
+
+    open OpenWithNative
+
+    let openFileWith fileName =
+        let mutable info =
+            OpenWithInfo(
+                cszFile = fileName,
+                cszClass = "",
+                oaifInFlags = (OpenWithFlags.ALLOW_REGISTRATION ||| OpenWithFlags.EXEC)
+            )
+        SHOpenWithDialog(IntPtr.Zero, &info)
+
 type IOperatingSystem =
     abstract member OpenFile: Path -> Result<unit, exn>
     abstract member OpenFileWith: Path -> Result<unit, exn>
@@ -20,9 +58,7 @@ type OperatingSystem() =
 
         member this.OpenFileWith path =
             tryResult <| fun () ->
-                let args = sprintf "shell32.dll, OpenAs_RunDLL %s" (wpath path)
-                ProcessStartInfo("rundll32.exe", args, WorkingDirectory = wpath path.Parent)
-                |> Process.Start |> ignore
+                OsInterop.openFileWith (wpath path) |> ignore
 
         member this.OpenExplorer node =
             match node.Type with

ca69de876d7bd93d445020bd5ff57cfc25f1c693
Matthew Miller <matthew.miller@acadian.com>
2019-04-25 18:48:45 -0500

    Adds message and logging for non-crash errors.

--- Koffee/Program.fs
+++ Koffee/Program.fs
@@ -4,17 +4,19 @@ open System.Windows
 open VinylUI.Wpf
 open ProgramOptions
 
-let logCrash (e: exn) =
+let logError isCrash (e: exn) =
+    let typ = if isCrash then "crash" else "error"
     let timestamp = System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss")
-    let logFile = Path.KoffeeData.Join(sprintf "crash_%s.log" timestamp).Format Windows
+    let logFile = Path.KoffeeData.Join(sprintf "%s_%s.log" typ timestamp).Format Windows
     System.IO.File.WriteAllText(logFile, string e)
     let msg =
-        "Sorry! An unexpected error caused Koffee to crash:\n\n" +
+        sprintf "Sorry! An unexpected error %s:\n\n"
+                (if isCrash then "caused Koffee to crash" else "occurred in Koffee") +
         sprintf "%s\n\n" e.Message +
         sprintf "This error has been logged to: \n%s\n\n" logFile +
         "Please report this as an issue on Koffee's GitHub project.\n\n" +
         "https://github.com/mattstermiller/koffee/issues"
-    MessageBox.Show(msg, "Crash!", MessageBoxButton.OK, MessageBoxImage.Error) |> ignore
+    MessageBox.Show(msg, sprintf "Koffee %s!" typ, MessageBoxButton.OK, MessageBoxImage.Error) |> ignore
 
 [<EntryPoint>]
 [<System.STAThread>]
@@ -35,7 +37,8 @@ let main args =
 #if DEBUG
     run ()
 #else
+    VinylUI.Framework.setErrorHandler (logError false)
     try run ()
-    with e -> logCrash e
+    with e -> logError true e
 #endif
     0

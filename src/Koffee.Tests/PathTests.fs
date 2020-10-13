ee554bc7e54753fa5886ca972add07ee8fbd97e8
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-10-13 22:38:24 +0200

    Refactored test

--- src/Koffee.Tests/PathTests.fs
+++ src/Koffee.Tests/PathTests.fs
@@ -137,30 +137,27 @@ let ``TypeConverter from string`` () =
     :?> Path
     |> shouldEqual expected
 
-[<TestCase(@"C:\Sample", @"C:\Sample", 0)>]
-[<TestCase(@"C:\Sample1", @"C:\Sample2", -1)>]
-[<TestCase(@"C:\Sample2", @"C:\Sample1", 1)>]
-let ``Compare Windows paths`` aPath bPath expected =
+let testComparability aPath bPath expected =
     let aComp = parseForTest aPath :> IComparable
     let bComp = parseForTest bPath :> IComparable
     aComp.CompareTo bComp |> shouldEqual expected
     bComp.CompareTo aComp |> shouldEqual -expected
+
+[<TestCase(@"C:\Sample", @"C:\Sample", 0)>]
+[<TestCase(@"C:\Sample1", @"C:\Sample2", -1)>]
+[<TestCase(@"C:\Sample2", @"C:\Sample1", 1)>]
+let ``Compare Windows paths`` aPath bPath expected =
+    testComparability aPath bPath expected
 
 [<TestCase(@"/c/Sample", @"/c/Sample", 0)>]
 [<TestCase(@"/c/Sample1", @"/c/Sample2", -1)>]
 [<TestCase(@"/c/Sample2", @"/c/Sample1", 1)>]
 let ``Compare Unix paths`` aPath bPath expected =
-    let aComp = parseForTest aPath :> IComparable
-    let bComp = parseForTest bPath :> IComparable
-    aComp.CompareTo bComp |> shouldEqual expected
-    bComp.CompareTo aComp |> shouldEqual -expected
+    testComparability aPath bPath expected
 
 [<TestCase(@"C:/Sample", @"/c/Sample", 0)>]
 [<TestCase(@"C:/Sample1", @"/c/Sample2", -1)>]
 [<TestCase(@"C:/Sample2", @"/c/Sample1", 1)>]
 let ``Compare Windows and Unix paths`` aPath bPath expected =
-    let aComp = parseForTest aPath :> IComparable
-    let bComp = parseForTest bPath :> IComparable
-    aComp.CompareTo bComp |> shouldEqual expected
-    bComp.CompareTo aComp |> shouldEqual -expected
+    testComparability aPath bPath expected
 

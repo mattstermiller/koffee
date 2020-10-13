ccc3e44161106edfee4cf8fb075cc5b298761f45
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-10-13 20:53:07 +0200

    Added history serializing test

---
+++ src/Koffee.Tests/HistoryTests.fs
@@ -0,0 +1,30 @@
+﻿module Koffee.HistoryTests
+
+open NUnit.Framework
+open FsUnitTyped
+open Newtonsoft.Json
+
+[<Test>]
+let ``History can be serialized and deserialized`` () =
+    let history =
+        { History.Default with
+            PathSort = Map.ofList [
+                (@"C:\Some\Path", {Sort = SortField.Name; Descending = true})
+                (@"C:\Some\Other\Path", {Sort = SortField.Modified; Descending = false})
+                (@"C:\Some\Third\Path", {Sort = SortField.Size; Descending = false})
+            ]
+            NetHosts = [
+                "Some net host"
+            ]
+            Paths = [
+                (Path.Parse "C:\Some\Path").Value
+            ]
+            Searches = [
+                ("downloads", false, false)
+                ("images", false, false)
+            ]
+        }
+    let converters = FSharpJsonConverters.getAll ()
+    let serialized = JsonConvert.SerializeObject(history, Formatting.Indented, converters)
+    let deserialized = JsonConvert.DeserializeObject<History>(serialized, converters)
+    deserialized |> shouldEqual history

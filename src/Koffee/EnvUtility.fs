82839e32353285d55ad5fb7817de26fd2c44d13e
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-10-25 20:22:12 +0100

    Renamed and moved function

--- src/Koffee/EnvUtility.fs
+++ src/Koffee/EnvUtility.fs
@@ -4,20 +4,20 @@ open System.Text.RegularExpressions
 
 let private subRegex = new Regex(@"%([^%]*)%", RegexOptions.Compiled)
 
+let private fastCheckMayContainSub (str: string) =
+    str.IndexOf '%' <> -1
+
 /// Substitutes %variables% in string using IOperatingSystem.GetEnvironmentVariable
 ///
-/// Example input: "Hello %target%!"
+/// Example input: "Hello %TARGET%!"
 /// Outputs: "Hello world!"
-/// (where "target" variable is set to "world")
+/// (where "TARGET" variable is set to "world")
 let subEnvVars (os: IOperatingSystem) str =
-    let mayContainSubstitution (str: string) =
-        str.IndexOf '%' <> -1
-
     let matchEval (m: Match) =
         os.GetEnvironmentVariable m.Groups.[1].Value
         |> Option.defaultValue m.Value
 
-    if mayContainSubstitution str then
+    if fastCheckMayContainSub str then
         subRegex.Replace(str, matchEval)
     else
         str

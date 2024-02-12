20e6c917415678734d7fa091453262621e448887
Matt Miller <miller.mattster@gmail.com>
2024-02-11 19:21:57 -0600

    Refactors OpenPath tests

--- src/Koffee.Tests/MainNavTests_OpenPath.fs
+++ src/Koffee.Tests/MainNavTests_OpenPath.fs
@@ -1,101 +1,78 @@
 module Koffee.MainNavTests_OpenPath
 
 open NUnit.Framework
-open Acadian.FSharp
 open Koffee.Main
-
-type PathCase =
-    | Same
-    | Different
-    | Inaccessible
-
-type TestCase = {
-    GetPath: PathCase
-    Select: SelectType
-    ExpectedCursor: int option
-}
-
-let model =
-    { testModel with
-        Items = [
-            createFolder "/c/file 1"
-            createFolder "/c/file 2"
-            createFolder "/c/file 3"
-        ]
-        Location = createPath "/c"
-        Cursor = 2
-        History = testModel.History |> History.withFolderPath 9 (createPath "/c")
-    }
-
-let test case =
-    let fs = FakeFileSystem [
-        folder "different" [
-            file "diff1"
-            file "diff2"
-        ]
-        folder "inaccessible" []
-        file "file"
-        file "newfile"
-    ]
-    fs.AddExn false ex "/c/inaccessible"
-
-    let path =
-        match case.GetPath with
-        | Same -> "/c"
-        | Different -> "/c/different"
-        | Inaccessible -> "/c/inaccessible"
-        |> createPath
-    let res = Nav.openPath fs path case.Select model
-
-    let expected =
-        match case.GetPath with
-        | Same ->
-            let items = fs.ItemsIn "/c"
-            Ok (
-                { model with
-                    Directory = items |> sortByPath
-                    Items = items
-                    Cursor = case.ExpectedCursor |? model.Cursor
-                }
-                |> MainModel.withLocation path
-            )
-        | Different ->
-            let items = fs.ItemsIn "/c/different"
-            Ok (
-                { model with
-                    Directory = items |> sortByPath
-                    Items = items
-                    Cursor = case.ExpectedCursor |? model.Cursor
-                    BackStack = (model.Location, model.Cursor) :: model.BackStack
-                    ForwardStack = []
-                    History = model.History |> History.withFolderPath 9 path
-                }
-                |> MainModel.withLocation path
-            )
-        | Inaccessible ->
-            Error (MainStatus.CouldNotOpenPath (path, model.PathFormat, ex))
-
-    assertAreEqual expected res
 
 [<TestCase(false)>]
 [<TestCase(true)>]
 let ``Opening a valid path updates model correctly`` setCursor =
-    test { GetPath = Different
-           Select = if setCursor then SelectIndex 1 else SelectNone
-           ExpectedCursor = Some (if setCursor then 1 else 0) }
+    let fs = FakeFileSystem [
+        folder "different" [
+            file "diff1"
+            file "diff2"
+            file "diff3"
+        ]
+        file "file"
+    ]
+    let path = createPath "/c/different"
+    let select = if setCursor then SelectIndex 2 else SelectNone
+    let model = testModel
+
+    let actual = Nav.openPath fs path select model |> assertOk
+
+    let expectedItems = fs.ItemsIn (string path)
+    let expected =
+        { model with
+            Directory = expectedItems |> sortByPath
+            Items = expectedItems
+            Cursor = if setCursor then 2 else 0
+            BackStack = (model.Location, model.Cursor) :: model.BackStack
+            ForwardStack = []
+        }
+        |> MainModel.withLocation path
+        |> withLocationOnHistory
+    assertAreEqual expected actual
 
 [<TestCase(false)>]
 [<TestCase(true)>]
 let ``Opening same path does not modify navigation history`` setCursor =
-    test { GetPath = Same
-           Select = if setCursor then SelectIndex 1 else SelectNone
-           ExpectedCursor = if setCursor then Some 1 else None }
+    let fs = FakeFileSystem [
+        folder "different" []
+        file "file1"
+        file "file2"
+    ]
+    let path = createPath "/c"
+    let select = if setCursor then SelectIndex 2 else SelectNone
+    let model = testModel
+
+    let actual = Nav.openPath fs path select model |> assertOk
+
+    let expectedItems = fs.ItemsIn "/c"
+    let expected =
+        { model with
+            Directory = expectedItems |> sortByPath
+            Items = expectedItems
+            Cursor = if setCursor then 2 else 0
+        }
+        |> MainModel.withLocation path
+        |> withLocationOnHistory
+    assertAreEqual expected actual
+
 
 [<Test>]
 let ``Opening a path that throws on GetItems sets error status only``() =
-    test { GetPath = Inaccessible
-           Select = SelectIndex 1
-           ExpectedCursor = None }
+    let fs = FakeFileSystem [
+        folder "inaccessible" []
+        file "file"
+    ]
+    let path = createPath "/c/inaccessible"
+    fs.AddExnPath false ex path
+    let model = testModel
+
+    let res = Nav.openPath fs path SelectNone model
+
+    let expected = Error (MainStatus.CouldNotOpenPath (path, model.PathFormat, ex))
+    assertAreEqual expected res
 
 [<Test>]
 let ``Open Parent when in folder opens parent and selects folder that was open`` () =

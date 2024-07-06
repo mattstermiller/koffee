7be0b04aba64b9180e3146a4ec8366faec672358
Matt Miller <miller.mattster@gmail.com>
2024-07-06 08:23:55 -0500

    Fixes possible cast error in IsCustomRowSelectionConverter
    Observed error during recursive search attempting to cast MS.Internal.NamedObject to IEnumerable

--- src/KoffeeUI/IsCustomRowSelectionConverter.cs
+++ src/KoffeeUI/IsCustomRowSelectionConverter.cs
@@ -9,13 +9,14 @@ namespace KoffeeUI {
     /// Given an item and sequence of selected items, returns whether the item is "selected".
     /// </summary>
     public class IsCustomSelectionConverter : IMultiValueConverter {
-        private static bool IsItemInSequence(object item, IEnumerable<object> selectedItems) =>
-            selectedItems.Contains(item);
-
-        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) =>
-            values.Length >= 2
-                ? (object)IsItemInSequence(values[0], (IEnumerable<object>)values[1])
-                : null;
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) {
+            if (values.Length >= 2) {
+                var item = values[0];
+                var selectedItems = values[1] as IEnumerable<object>;
+                return selectedItems?.Contains(item);
+            }
+            return null;
+        }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) =>
             throw new NotImplementedException();
